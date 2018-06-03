1d803c1f86619d7e21bfe2103c61a498c3654907
tmunson <tmunson@wsu.edu>
2018-06-02 19:08:57 -0700

    More tests

--- raiderio_test.go
+++ raiderio_test.go
@@ -2,10 +2,18 @@ package raideriogo
 
 import "testing"
 
-func TestEndpointCharacter(t *testing.T) {
-	var expectedURL = "https://raider.io/api/v1/characters/profile?region=us&realm=testRealm&name=testName"
+func TestEndpointCharacterNoFields(t *testing.T) {
+	var expectedURL = "https://raider.io/api/v1/characters/profile?region=testRegion&realm=testRealm&name=testName"
 
-	if EndpointCharacter("us", "testRealm", "testName") != expectedURL {
-		t.Fatal("TestEndpointCharacter failed.")
+	if EndpointCharacter("testRegion", "testRealm", "testName", "") != expectedURL {
+		t.Fatal("String mismatch.")
+	}
+}
+
+func TestEndpointCharacterWithFields(t *testing.T) {
+	var expectedURL = "https://raider.io/api/v1/characters/profile?region=testRegion&realm=testRealm&name=testName&fields=gear%2Cguild%2Cmythic_plus_scores"
+
+	if EndpointCharacter("testRegion", "testRealm", "testName", "gear,guild,mythic_plus_scores") != expectedURL {
+		t.Fatal("String mismatch.")
 	}
 }
